d0ed735e9d31f4caf8f064107af9d39fce5087bc
Ivan Kremnev <iakremnev@mail.ru>
2026-03-06 16:31:29 +0200

    query all countries with effect hook

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
 import Filter from './Filter'
+import restcountries from './services/restcountries'
 
 const App = () => {
 
     const [filterString, setFilterString] = useState("")
+    const [countries, setCountries] = useState([])
 
     const handleFilterInput = (event) => {
         console.log(event.target.value)
         setFilterString(event.target.value)
     }
+
+    useEffect(() => setCountries(restcountries.getAll()), [])
 
     return (
         <Filter filterQuery={filterString}  handleInput={handleFilterInput} />
