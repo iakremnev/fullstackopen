9b130bbef28457111813cb06079bdecd7c67eb4a
Ivan Kremnev <iakremnev@mail.ru>
2026-03-12 19:52:53 +0200

    Person custom json formatter

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -8,6 +8,13 @@ const PersonSchema = new mongoose.Schema({
   name: String,
   number: String
 })
+PersonSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 const Person = new mongoose.model('Person', PersonSchema)
 
