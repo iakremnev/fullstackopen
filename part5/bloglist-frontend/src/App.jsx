57eba4255f26c7443bad35eafcd6af46588e711e
Ivan Kremnev <iakremnev@mail.ru>
2026-05-21 18:45:03 +0300

    add login tp bloglist-frontend

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,37 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([]);
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    useEffect(() => {
+        blogService.getAll().then((blogs) => setBlogs(blogs));
+    }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+    const handleLogin = async (username, password) => {
+        console.log(`Username: ${username}, password: ${password}`);
+        const userData = await loginService.login(username, password);
+        setUser(userData);
+    };
 
-export default App
+    return (
+        <div>
+            <h2>blogs</h2>
+            {!user && <LoginForm handleLogin={handleLogin} />}
+            {user && (
+                <>
+                    <p>{user.name} logged in</p>
+                    {blogs.map((blog) => (
+                        <Blog key={blog.id} blog={blog} />
+                    ))}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default App;
---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const LoginForm = ({ handleLogin }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setUsername("")
+        setPassword("")
+        handleLogin(username, password);
+    };
+
+    return (
+        <div>
+            <form type="submit" onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        username:
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(event) =>
+                                setUsername(event.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        password:
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(event) =>
+                                setPassword(event.target.value)
+                            }
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button type="submit">Sign In</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default LoginForm;
